9f70d471e2d18ef47d5ff787bd22d4c829aad0ef
Funnyyanne <lochienpin@gmail.com>
2025-01-05 00:11:25 +0800

    feat:合约函数

---
+++ fullstack/Funnyyanne/week1/profile_list/src/Contract.ts
@@ -0,0 +1,3 @@
+export const PackageId ="0x894b01eda99126b48f8ba0e1beb222019e28319cfd20910dfd41e652bcd485ff";
+export const ContractName ="";
+export const State = "0xad81291c092c932d421334dd33a0bec92870bf4389550f814d14ed2bb52fda3c";
